e45b1b9c1009444b7fe2240572f2c3bdb01ce7e8
Vysali-Devabhaktuni <77681075+Vysali-Devabhaktuni@users.noreply.github.com>
2022-12-21 13:14:51 +0530

    Add files via upload

--- server/modules/db_connect.js
+++ server/modules/db_connect.js
@@ -1,29 +1,37 @@
-require('dotenv').config();
-const mysql = require('mysql2');
 
-const con = mysql.createConnection({
-  host: process.env.MYSQL_HOST,
-  user: process.env.MYSQL_USERNAME,
-  password: process.env.MYSQL_PASSWORD,
-  database: process.env.MYSQL_DB
-});
+// getUsers button 
+/* document.getElementById("btn-users").addEventListener('click', getUsers);
+ function getUsers() {
+   fetch("http://localhost:3000/users/")
+   .then((res)=> res.json())
+   .then((data) => console.log(data))
+   .catch((err)=> console.log(err))
+ }*/ 
 
-const query = (sql, binding) => {
-  return new Promise((resolve, reject) => {
-    con.query(sql, binding, (err, result, fields)=> {
-      if(err) reject(err);
-      resolve(result);
-    });
-  })
-}
-
-con.connect(function (err){
-  if(err) throw err;
-  console.log("Connected!");
-  con.query("CREATE DATABASE IF NOT EXISTS myproject_db", function (err, result){
-    if(err) throw err;
-    console.log("Database created");
-  });
-});
-
-module.exports = {con, query};
+ require('dotenv').config();
+ const mysql = require('mysql2');
+ 
+ const con = mysql.createConnection({
+   host: process.env.MYSQL_HOST,
+   user: process.env.MYSQL_USERNAME,
+   password: process.env.MYSQL_PASSWORD,
+   database: process.env.MYSQL_DB
+ })
+ 
+ const query = (sql, binding) => {
+   return new Promise((resolve, reject) => {
+     con.query(sql, binding, (err, result, fields)=> {
+       if(err) reject(err);
+       resolve(result);
+     })
+   })
+ }
+ con.connect(function (err){
+   if(err) throw err;
+   console.log("connected successfully");
+   con.query(`CREATE DATABASE IF NOT EXISTS ${process.env.MYSQL_DB}`, function (err, result){
+        if(err) throw err;
+        console.log("Database created successfully ");
+     });
+ });
+ module.exports = {con, query};
